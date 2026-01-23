5c86195507c1f452f35d70701593b6649fefa3b4
Aldordzvn <aldordzvn2001@gmail.com>
2026-01-22 18:14:17 -0700

    Primer avance de header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { faMoon} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  imports: [FontAwesomeModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  moonIcon = faMoon;
+}
