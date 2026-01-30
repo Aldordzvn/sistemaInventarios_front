7f8df0a94a73208474fea482fc78669057939641
Aldordzvn <aldordzvn2001@gmail.com>
2026-01-30 11:33:13 -0700

    Se añade la vista de about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {  faLaptopCode, faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faAngular, faJava, faSass, faHtml5 } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  gitHubIcon = faGithub;
+  linkedInIcon = faLinkedin;
+  laptopCode = faLaptopCode;
+  dataBaseIcon = faDatabase;
+  angularIcon = faAngular;
+  javaIcon = faJava;
+  sassIcon = faSass;
+  htmlIcon = faHtml5;
+
 
 }
